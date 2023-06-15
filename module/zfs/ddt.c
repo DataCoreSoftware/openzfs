e1f6fc91f9e2e6cf8921608a93a76197683a59a8
Rob Norris <rob.norris@klarasystems.com>
2023-06-15 17:19:41 +1000

    ddt: cleanup the stats & histogram code
    Both the API and the code were kinda mangled and I was really struggling
    to follow it. The worst offender was the old ddt_stat_add(); after
    fixing it up the rest of the changes are mostly knock-on effects and
    targets of opportunity.
    
    Note that the old ddt_stat_add() was safe against overflows - it could
    produce crazy numbers, but the compiler wouldn't do anything stupid. The
    assertions in ddt_stat_sub() go a lot of the way to protecting against
    this; getting in a position where overflows are a problem is definitely
    a programming error.
    
    Also expanding ddt_stat_add() and ddt_histogram_empty() produces less
    efficient assembly. I'm not bothered about this right now though; these
    should not be hot functions, and if they are we'll optimise them later.
    If we have to go back to the old form, we'll comment it like crazy.
    
    Finally, I've removed the assertion that the bucket will never be
    negative, as it will soon be possible to have entries with zero
    refcounts: an entry for a block that is no longer on the pool, but is on
    the log waiting to be synced out. It might be better to have a separate
    bucket for these, since they're still using real space on disk, but
    ultimately these stats are driving UI, and for now I've chosen to keep
    them matching how they've looked in the past, as well as match the
    operators mental model - pool usage is managed elsewhere.
    
    Reviewed-by: Alexander Motin <mav@FreeBSD.org>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Rob Norris <rob.norris@klarasystems.com>
    Sponsored-by: Klara, Inc.
    Sponsored-by: iXsystems, Inc.
    Closes #15895

--- module/zfs/ddt.c
+++ module/zfs/ddt.c
@@ -932,7 +932,18 @@ ddt_lookup(ddt_t *ddt, const blkptr_t *bp)
 	dde->dde_class = class;	/* will be DDT_CLASSES if no entry found */
 
 	if (error == 0) {
-		ddt_stat_update(ddt, dde, -1ULL);
+		/*
+		 * The histograms only track inactive (stored) blocks.
+		 * We've just put an entry onto the live list, so we need to
+		 * remove its counts. When its synced back, it'll be re-added
+		 * to the right one.
+		 */
+		ddt_histogram_t *ddh =
+		    &ddt->ddt_histogram[dde->dde_type][dde->dde_class];
+
+		ddt_lightweight_entry_t ddlwe;
+		DDT_ENTRY_TO_LIGHTWEIGHT(ddt, dde, &ddlwe);
+		ddt_histogram_sub_entry(ddt, ddh, &ddlwe);
 	}
 
 	/* Entry loaded, everyone can proceed now */
@@ -1467,10 +1478,17 @@ ddt_sync_entry(ddt_t *ddt, ddt_entry_t *dde, dmu_tx_t *tx, uint64_t txg)
 	if (total_refcnt != 0) {
 		dde->dde_type = ntype;
 		dde->dde_class = nclass;
-		ddt_stat_update(ddt, dde, 0);
+
 		if (!ddt_object_exists(ddt, ntype, nclass))
 			ddt_object_create(ddt, ntype, nclass, tx);
 		VERIFY0(ddt_object_update(ddt, ntype, nclass, dde, tx));
+
+		ddt_lightweight_entry_t ddlwe;
+		DDT_ENTRY_TO_LIGHTWEIGHT(ddt, dde, &ddlwe);
+
+		ddt_histogram_t *ddh =
+		    &ddt->ddt_histogram[ntype][nclass];
+		ddt_histogram_add_entry(ddt, ddh, &ddlwe);
 
 		/*
 		 * If the class changes, the order that we scan this bp
@@ -1480,8 +1498,6 @@ ddt_sync_entry(ddt_t *ddt, ddt_entry_t *dde, dmu_tx_t *tx, uint64_t txg)
 		 * traversing.)
 		 */
 		if (nclass < oclass) {
-			ddt_lightweight_entry_t ddlwe;
-			DDT_ENTRY_TO_LIGHTWEIGHT(ddt, dde, &ddlwe);
 			dsl_scan_ddt_entry(dp->dp_scan,
 			    ddt->ddt_checksum, ddt, &ddlwe, tx);
 		}
